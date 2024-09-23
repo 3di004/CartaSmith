ce90f98149332627f4de1f0169b8852763d35c92
3di004 <134215014+3di004@users.noreply.github.com>
2024-09-23 15:00:38 -0300

    Added Carta-de-Smith.py

---
+++ Carta-de-Smith.py
@@ -0,0 +1,4 @@
+# Imports e Configurações ---------------------------------------------------------------------------------------------
+
+import numpy as np
+import matplotlib.pyplot as plt
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# CartaSmith
+# Projeto Colaborativo da Carta de Smith
+Disciplina: Ondas e Linhas
+
+Colaboradores:
+- Edileudo;
+- Adolfo;
+- Jales;
+- Pedro;
+- Emerson;
+- Caíque.
